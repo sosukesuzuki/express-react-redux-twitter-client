f8b00ccaa01ca63f7e5b0b58c23b61b511947b96
Sosuke Suzuki <aosukeke@gmail.com>
2017-01-09 01:15:32 +0900

    tweet & TL

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var twitterAPI = require('node-twitter-api');
+var Twitter = require('twitter');
+var util = require('util');
 
 var consumerKey = '7OGggcfImlerRKldxqIMjy6ok';
 var consumerSecret = 'pncbkLAxfO6X3B5qjedRJSQHpJi5zCfQNZDc8GkBqGvIyGZUdq';
@@ -15,9 +17,12 @@ var app = express();
 app.listen(3000);
 
 var _requestSecret;
-var _accessToken;
-var _accessSecret;
+var _accessToken = 'empty';
+var _accessSecret = 'empty';
 
+var client;
+
+//リクエストトークン発行
 app.get("/request-token", function(req, res) {
     twitter.getRequestToken(function(err, requestToken, requestSecret) {
         if (err)
@@ -25,10 +30,12 @@ app.get("/request-token", function(req, res) {
         else {
             _requestSecret = requestSecret;
             res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+            //リダイレクトしてaccess_tokenも発行
         }
     });
 });
 
+//requiest-token発行後のリダイレクト先
 app.get("/access-token", function(req, res) {
     var requestToken = req.query.oauth_token,
         verifier = req.query.oauth_verifier;
@@ -45,7 +52,40 @@ app.get("/access-token", function(req, res) {
                     _accessToken = accessToken;
                     _accessSecret = accessSecret;
                     console.log('Accesstoken: ' + _accessToken + '/Accesssecret: ' + _accessSecret);
+                    //token類が発行され終わったのでここでtwitterのクライアント変数に全部一気に代入するよ
+                    client = new Twitter({
+                        consumer_key: consumerKey,
+                        consumer_secret: consumerSecret,
+                        access_token_key: _accessToken,
+                        access_token_secret: _accessSecret
+                    });
                 }
             });
+    });
+});
+
+//後に続くパラメータをツイートします
+app.post('/status/update/:tweet', function(req, res){
+    client.post('statuses/update', {status: req.params.tweet},  function(error, tweet, response) {
+        if(!error) {
+            console.log(tweet);  // Tweet body.
+            console.log(response);
+            res.json({
+                message: 'tweet success!!'
+            })
+        }
+    });
+});
+
+//後に続くscreen_nameの人のTLをjsonでもらいます
+app.get('/statuses/user_timeline/:user', function(req, res){
+    client.get('statuses/user_timeline', {screen_name: req.params.user}, function(error, tweets, response){
+        if(!error){
+            res.send(tweets);
+        } else {
+            res.json({
+                message: 'error! tweet failured!!'
+            })
+        }
     });
 });
